ec6844f28d1b739e7cf5c1f32003dab2788a7d53
Parth5633 <parthbosamiya2007@gmail.com>
2026-02-04 18:37:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+function log(msg) {
+  const p = document.createElement("p");
+  p.innerText = msg;
+  document.body.append(p);
+}
+
+log("Script started");
+
 const pc = new RTCPeerConnection({
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
 });
@@ -12,86 +20,57 @@ localVideo.muted = true;
 remoteVideo.autoplay = true;
 
 document.body.append(localVideo, remoteVideo);
-
 remoteVideo.srcObject = remoteStream;
 
-// ---------- UI ----------
-const createBtn = document.createElement("button");
-createBtn.innerText = "Create Room";
-document.body.append(createBtn);
+const roomRef = firebase.database().ref("debug-room");
 
-const joinInput = document.createElement("input");
-joinInput.placeholder = "Enter Room ID";
-document.body.append(joinInput);
-
-const joinBtn = document.createElement("button");
-joinBtn.innerText = "Join Room";
-document.body.append(joinBtn);
-
-// ---------- Media ----------
-async function initMedia() {
-  localStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-  });
+async function start() {
+  log("Getting camera...");
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   localVideo.srcObject = localStream;
+  log("Camera OK");
 
   localStream.getTracks().forEach(t => pc.addTrack(t, localStream));
 
   pc.ontrack = e => {
     e.streams[0].getTracks().forEach(t => remoteStream.addTrack(t));
+    log("Remote stream received");
   };
-}
-
-// ---------- Signaling ----------
-async function setupRoom(roomId, isCaller) {
-  const roomRef = firebase.database().ref("rooms/" + roomId);
 
   pc.onicecandidate = e => {
     if (e.candidate) {
       roomRef.child("ice").push(e.candidate.toJSON());
+      log("ICE sent");
     }
   };
 
-  roomRef.child("ice").on("child_added", snap => {
-    pc.addIceCandidate(new RTCIceCandidate(snap.val()));
-  });
+  const offerSnap = await roomRef.child("offer").once("value");
 
-  if (isCaller) {
+  if (!offerSnap.exists()) {
+    log("I am caller");
     const offer = await pc.createOffer();
     await pc.setLocalDescription(offer);
     await roomRef.child("offer").set(offer);
 
     roomRef.child("answer").on("value", async snap => {
       if (snap.exists()) {
-        await pc.setRemoteDescription(
-          new RTCSessionDescription(snap.val())
-        );
+        log("Answer received");
+        await pc.setRemoteDescription(new RTCSessionDescription(snap.val()));
       }
     });
 
   } else {
-    const offer = (await roomRef.child("offer").once("value")).val();
-    await pc.setRemoteDescription(new RTCSessionDescription(offer));
-
+    log("I am answerer");
+    await pc.setRemoteDescription(new RTCSessionDescription(offerSnap.val()));
     const answer = await pc.createAnswer();
     await pc.setLocalDescription(answer);
     await roomRef.child("answer").set(answer);
   }
+
+  roomRef.child("ice").on("child_added", snap => {
+    pc.addIceCandidate(new RTCIceCandidate(snap.val()));
+    log("ICE received");
+  });
 }
 
-// ---------- Buttons ----------
-createBtn.onclick = async () => {
-  const roomId = Math.floor(Math.random() * 1000000).toString();
-  alert("Room ID: " + roomId);
-  await setupRoom(roomId, true);
-};
-
-joinBtn.onclick = async () => {
-  const roomId = joinInput.value.trim();
-  if (!roomId) return alert("Enter Room ID");
-  await setupRoom(roomId, false);
-};
-
-// ---------- Start ----------
-initMedia();
+start();
